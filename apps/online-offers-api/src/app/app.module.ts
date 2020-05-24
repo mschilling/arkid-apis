78ce5d8cfd42bded1acd909b51c8c07ae626f688
Michael Schilling <mschilling@move4mobile.com>
2020-05-24 16:39:37 +0200

    Initial Offers module

--- apps/online-offers-api/src/app/app.module.ts
+++ apps/online-offers-api/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { OffersModule } from './offers/offers.module';
 
 @Module({
   imports: [OffersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [OffersModule],
 })
 export class AppModule {}
--- apps/online-offers-api/src/app/offers/offers.service.ts
+++ apps/online-offers-api/src/app/offers/offers.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class OffersService {}
+export class OffersService {
+  async getAll(): Promise<any[]> {
+    return <Array<any>>[
+      { id: 1, title: 'Dagaanbieding 1' },
+      { id: 2, title: 'Dagaanbieding 2' },
+      { id: 3, title: 'Dagaanbieding 3' },
+    ];
+  }
+}
